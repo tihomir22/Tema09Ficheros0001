d867f4fa14a8de8d6941e3e60a412c95f95c8729
mati <mati@1smx>
2018-04-18 10:26:42 +0200

    primer commit

---
+++ Tema09Ficheros0001/src/tema09ficheros0001/Tema09Ficheros0001.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema09ficheros0001;
+
+import java.io.File;
+
+/**
+ *
+ * @author mati
+ */
+public class Tema09Ficheros0001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File f=new File("/home/mati/Documents/file.txt");
+    }
+    
+}
